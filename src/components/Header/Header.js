3e09b7b082a301fcf95d306340f2f6056e4004de
bassamkdev <bassamkazemi.dev@gmail.com>
2021-04-21 21:43:59 -0700

    fixed main-header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,20 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: center;
+  padding: 21px 32px;
+  padding-right: 170px;
+  gap: 48px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
